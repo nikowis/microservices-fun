e8eae7b922bad04be18bcfccdf550cef0c4751f9
nikowis <nikowis@gmail.com>
2022-04-21 23:02:56 +0200

    Consume streams from twitter

--- twitter-to-kafka-service/src/main/java/com/nikowis/microservices/twitter/to/kafka/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/nikowis/microservices/twitter/to/kafka/TwitterToKafkaServiceApplication.java
@@ -1,12 +1,25 @@
 package com.nikowis.microservices.twitter.to.kafka;
 
+import com.nikowis.microservices.twitter.to.kafka.runner.StreamRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TwitterToKafkaServiceApplication {
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    private final StreamRunner streamRunner;
+
+    public TwitterToKafkaServiceApplication(StreamRunner streamRunner) {
+        this.streamRunner = streamRunner;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        streamRunner.start();
     }
 }
---
+++ twitter-to-kafka-service/src/main/java/com/nikowis/microservices/twitter/to/kafka/runner/StreamRunner.java
@@ -0,0 +1,8 @@
+package com.nikowis.microservices.twitter.to.kafka.runner;
+
+import twitter4j.TwitterException;
+
+public interface StreamRunner {
+
+    void start() throws TwitterException;
+}
