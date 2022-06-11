b0a22c860678f346673a23406a4cf86c5a73ce19
nikowis <nikowis@gmail.com>
2022-06-11 14:43:33 +0200

    Create kafka-to-elastic module

---
+++ kafka-to-elastic-service/src/main/java/com/nikowis/tstreams/kafka/to/elastic/KafkaToElasticServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nikowis.tstreams.kafka.to.elastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.nikowis")
+public class KafkaToElasticServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaToElasticServiceApplication.class, args);
+    }
+}
