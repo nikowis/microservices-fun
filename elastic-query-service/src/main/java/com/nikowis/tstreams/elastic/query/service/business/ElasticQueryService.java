91be18cdf853b7c9a5f88c6ae7b746b4a4e4e176
nikowis <nikowis@gmail.com>
2022-07-02 16:54:52 +0200

    Elastic query service

---
+++ elastic-query-service/src/main/java/com/nikowis/tstreams/elastic/query/service/business/ElasticQueryService.java
@@ -0,0 +1,14 @@
+package com.nikowis.tstreams.elastic.query.service.business;
+
+import com.nikowis.tstreams.elastic.query.service.model.ElasticQueryServiceResponseModel;
+
+import java.util.List;
+
+public interface ElasticQueryService {
+
+    ElasticQueryServiceResponseModel getDocumentById(String id);
+
+    List<ElasticQueryServiceResponseModel> getDocumentByText(String text);
+
+    List<ElasticQueryServiceResponseModel> getAllDocuments();
+}
--- elastic/elastic-config/src/main/java/com/nikowis/tstreams/elastic/config/ElasticsearchConfig.java
+++ elastic/elastic-config/src/main/java/com/nikowis/tstreams/elastic/config/ElasticsearchConfig.java
@@ -16,7 +16,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.util.Objects;
 
 @Configuration
-@EnableElasticsearchRepositories(basePackages = "com.nikowis.tstreams.elastic.index.client.repository")
+@EnableElasticsearchRepositories(basePackages = "com.nikowis.tstreams.elastic")
 public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
 
     private final ElasticConfigData elasticConfigData;
